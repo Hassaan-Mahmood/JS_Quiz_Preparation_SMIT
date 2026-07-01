6b967a18e6823bb93943895eec920e62d6686dee
Hassaan Mahmood <hassaan2980@gmail.com>
2026-07-01 19:29:33 +0000

    1 to 5 Tasks completed

---
+++ casing.js
@@ -0,0 +1,43 @@
+// Task 1: Convert to lowercase
+// let userText = prompt("Enter Your Name");
+// userText = userText.toLowerCase();
+// console.log("Lowercase: " + userText);
+
+// Task 2: Convert to uppercase
+// let userText = prompt("Enter Your Name");
+// let upperCaseText = userText.toUpperCase();
+// // console.log(userText);
+// console.log("Uppercase: " + upperCaseText);
+
+// Task 3: Greeeting the user 
+// let userName = prompt("Enter Your Name");
+// let greeting = "Hello, " + userName + "! Welcome to our website.";
+// greeting= greeting.toUpperCase();
+// console.log(greeting);
+
+// Task 4: City Check;
+// let userCity = prompt("Enter Your City");
+// userCity = userCity.toLowerCase();
+// let cities = ["karachi", "lahore", "islamabad", "quetta", "peshawar"];
+// let cityFound = false;
+// for (let i = 0; i < cities.length; i++) {
+//     if (userCity === cities[i]){
+//         cityFound = true;
+//     }
+// }
+// if (cityFound === true) {
+//     console.log ("City Found: " + userCity);
+// } else {
+//     console.log("City Not Found: " + userCity);
+// }
+
+// Task 5: Password Check
+// let password = "saylani123";
+// let userPassword = prompt("Enter your password");
+// userPassword = userPassword.toLowerCase();
+// if (userPassword === password){
+//     console.log("Password is correct");
+// } else {
+//     console.log("Password is incorrect");
+// }
+
